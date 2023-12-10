c4b0a860b139e25f2d151f9ad4625620d89508e5
Tinam Durge <tinamdurge02@gmail.com>
2023-12-10 23:01:36 +0530

    Added: Ability to add main function

--- src/BinarySearchTreeMain.java
+++ src/BinarySearchTreeMain.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class BinarySearchTreeMain {
+public class BinarySearchTreeMain {
+    public static void main(String[] args) {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.addData(56);
+        binarySearchTree.addData(30);
+        binarySearchTree.addData(70);
+        binarySearchTree.printData();
+
+
+    }
 }
